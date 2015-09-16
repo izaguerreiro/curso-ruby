8abb8267177ff350e8efb7ef1c62145708800588
Izabela Guerreiro <izaguerreiro@gmail.com>
2015-09-15 22:37:13 -0300

    fix

--- rails/job-board/app/controllers/application_controller.rb
+++ rails/job-board/app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  private
+  def current_company
+  	@current_company ||= if session[:company_id]
+  		Company.find_by_id(session[:company_id])
+  	end
+  end
+  helper_method :current_company
 end
