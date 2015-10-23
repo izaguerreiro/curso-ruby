4b37d44ba013a9a66ffc461cb7a65785f26cf06a
izaguerreiro <izaguerreiro@gmail.com>
2015-10-23 10:03:17 -0200

    fix

--- rails/job-board/app/controllers/application_controller.rb
+++ rails/job-board/app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :set_locale
 
   private
   def current_company
@@ -8,4 +9,13 @@ class ApplicationController < ActionController::Base
   	end
   end
   helper_method :current_company
+
+  def set_locale
+  	locale = params[:locale] || cookies[:locale]
+
+  	if locale.present?
+  		I18n.locale
+  		cookies[:locale] = {value: locale, expires: 30.days.from_now}
+  	end
+  end
 end
